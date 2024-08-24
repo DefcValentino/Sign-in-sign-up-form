8aaddc9fbeead51a53fb29ca2537c7475fc71a19
Ebube Valentino <164953300+DefcValentino@users.noreply.github.com>
2024-08-24 04:40:55 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+let signinBtn = document.getElementById('signinBtn');
+let signupBtn = document.getElementById('signupBtn');
+let namefield = document.getElementById('namefield');
+let title = document.getElementById('title');
+
+signinBtn.onclick = function() {
+    namefield.style.maxHeight = '0';
+    title.innerHTML = 'Sign in';
+    signupBtn.classList.add('disable');
+    signinBtn.classList.remove('disable');
+}
+
+signupBtn.onclick = function() {
+    namefield.style.maxHeight = '60px';
+    title.innerHTML = 'Sign up';
+    signupBtn.classList.remove('disable');
+    signinBtn.classList.add('disable');
+}
+
+
+
+
